a1dcf0bc7f1f37dde930c3cc1b5bb376bfd02b0f
ThejollyWalrus <47336151+ThejollyWalrus@users.noreply.github.com>
2020-04-07 20:51:34 -0700

    Add files via upload

---
+++ lightDim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightDim : MonoBehaviour
+{
+   // public Light lightLow;
+    float darker = 0.25f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //lightLow.intensity -= 1;
+    }
+    void OnTriggerEnter(Collider blocker)
+    {
+        if (blocker.CompareTag("box"))
+        {
+          //  lightLow.intensity -= darker;
+        }
+    }
+}
---
+++ lightingActive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightingActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+
+
+        public GameObject NameOfGameObjectThatYouWantToActivate;
+        public Light lighter;
+        void Start()
+        {
+            NameOfGameObjectThatYouWantToActivate.SetActive(true);
+            NameOfGameObjectThatYouWantToActivate.SetActive(false);
+        }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lighter.intensity <= 0)
+            NameOfGameObjectThatYouWantToActivate.SetActive(true);
+    }
+}
